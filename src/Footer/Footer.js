e21280709d390ea2d26a71416db86794ae931fc0
riccardopalmieri95 <68059318+riccardopalmieri95@users.noreply.github.com>
2020-10-07 11:21:28 +0200

    added footer hrefs

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
 import {fab, faTwitterSquare, faFacebook, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import Link from "@material-ui/core/Link";
 
 class Footer extends Component {
   render() {
     return <footer id="footer">
+      <h3> Mi puoi trovare anche qui:  </h3>
       <ul className="icons">
-        <li><FontAwesomeIcon icon={faLinkedin} /></li>
-        <li><FontAwesomeIcon icon={faGithub} /></li>
+        <li><a href="https://www.linkedin.com/in/riccardopalmieri-95"> <FontAwesomeIcon icon={faLinkedin} /></a> </li>
+        <li><a href="https://github.com/superpalmi"> <FontAwesomeIcon  icon={faGithub} /> </a> </li>
       </ul>
       <p className="copyright">&copy; Riccardo Palmieri. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a
           href="https://unsplash.com">Unsplash</a>.</p>
